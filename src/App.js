46d0263b55b3739dc0ed41cb571849e18b26f1f1
tuyenhuynh20011 <83766834+tuyenhuynh20011@users.noreply.github.com>
2022-08-09 16:07:46 +0700

    cm lab07_02

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import Main from './components/maincomponent';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+import './App.css';
+
+const store = ConfigureStore();
+
+class App extends Component{
+
+
+  render(){
+    return (
+      <div className="App">
+          <Provider store={store}>
+            <BrowserRouter>
+              <div className="App">
+                <Main />
+              </div>
+            </BrowserRouter>
+          </Provider>
+      </div>
+    );
+  }
+ }
+export default App;
