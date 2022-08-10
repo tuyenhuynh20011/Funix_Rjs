7f793d120b8f02f7153e625a5f62c7fa7e5770b1
tuyenhuynh20011 <83766834+tuyenhuynh20011@users.noreply.github.com>
2022-08-10 10:12:59 +0700

    commit lab10_04

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,155 @@
+import React, {Component} from 'react';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem, Label,
+    Modal, ModalHeader, ModalBody, Button, Row, Col } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import { Control, LocalForm } from 'react-redux-form';
+import dateFormat from "dateformat";
+import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+
+       
+function RenderComments({comments, postComment, dishId}) {
+    const comment1 = comments.map((comment1)=>{
+        return(
+            <div>
+            <p>{comment1.comment}</p>
+            <p>{"-- "+comment1.author+ ", "+dateFormat(comment1.date,"dd/mm/yyyy")}</p>
+            </div>
+        )
+    });
+    return(
+        <div className="col-12 col-md-5 m-1">
+            <h4 style={{textAlign: "left"}}>Comments</h4>
+                    {comment1}
+                    <CommentForm dishId={dishId} postComment={postComment} />
+        </div>
+    )
+}
+
+class CommentForm extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        
+        this.state = {
+          isNavOpen: false,
+          isModalOpen: false
+        };
+    }
+
+    toggleModal() {
+        this.setState({
+          isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    handleSubmit(values) {
+        this.toggleModal();
+        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
+    }
+
+    render() { 
+        return(
+        <div>
+            <Button outline onClick={this.toggleModal}><span className="fa fa-pencil fa-lg"></span> Submit Comment</Button>
+            <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+            <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+            <ModalBody>
+                <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                    <Row className="form-group">
+                        <Col>
+                        <Label htmlFor="rating">Rating</Label>
+                        <Control.select model=".rating" id="rating" className="form-control"  defaultValue={1}>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </Control.select>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Label htmlFor="author">Your Name</Label>
+                            <Control.text model=".author" id="author"
+                                     className="form-control" />
+
+                        </Col>
+                    </Row>
+
+                    <Row className="form-group">
+                        <Col>
+                        <Label htmlFor="comment">Comment</Label>
+                        <Control.textarea model=".comment" id="comment"
+                                    rows="6" className="form-control" />
+                        </Col>
+                    </Row>
+                    <Button type="submit" className="bg-primary">
+                        Submit
+                    </Button>
+                </LocalForm>
+            </ModalBody>
+           </Modal>
+        </div>
+        );
+    }
+
+}
+ 
+const DishDetail = (props) => {
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish != null) 
+    return(
+        <div className="container">
+           <div className="row">
+                    <Breadcrumb>
+                    <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
+                </div> 
+        <div className="row">
+            <div  className="col-12 col-md-5 m-1">
+                <Card>
+                    <CardImg top src={baseUrl + props.dish.image} alt={props.dish.name} />  
+                    <CardBody>
+                        <CardTitle>{props.dish.name}</CardTitle>
+                        <CardText>{props.dish.description}</CardText>
+                    </CardBody>
+                </Card>
+                </div>
+                <RenderComments comments = {props.comments} postComment= {props.postComment} dishId={props.dish.id}/>
+        </div>
+        </div>
+    );
+else
+    return(
+        <div></div>
+    );
+}
+export default DishDetail;
